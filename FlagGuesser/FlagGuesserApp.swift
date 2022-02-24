40f923e24adbb5efbc2fadad849161e3bd2eab3e
alusch7 <luschera@mcmaster.ca>
2022-02-23 21:15:04 -0500

    Initial Commit

---
+++ FlagGuesser/FlagGuesserApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagGuesserApp.swift
+//  FlagGuesser
+//
+//  Created by Alden Luscher on 2022-02-23.
+//
+
+import SwiftUI
+
+@main
+struct FlagGuesserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
